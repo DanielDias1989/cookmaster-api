54ce2dbed57193aaeaa8882136142f017c4cc10d
Daniel Dias <daniel.aguiardias@yahoo.com.br>
2026-09-04 14:42:31 -0300

    Commit inicial

---
+++ src/model/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+const connection = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+module.exports = connection;
