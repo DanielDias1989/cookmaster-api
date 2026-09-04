41927af41b6df9ea69c0d799b6314db1f8738ae5
Daniel Dias <daniel.aguiardias@yahoo.com.br>
2026-09-04 15:11:33 -0300

    Ajustes de conexão

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST || process.env.MYSQL_HOST,
+  port: process.env.MYSQLPORT || process.env.MYSQL_PORT,
+  user: process.env.MYSQLUSER || process.env.MYSQL_USER,
+  password: process.env.MYSQLPASSWORD
+    || process.env.MYSQL_PASSWORD
+    || process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQLDATABASE
+    || process.env.MYSQL_DATABASE
+    || process.env.MYSQL_DATABASE_NAME,
 });
 module.exports = connection;
